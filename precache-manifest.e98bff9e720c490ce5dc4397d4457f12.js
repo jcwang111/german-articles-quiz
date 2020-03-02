1b89bfabfeabfd865303df855828d56a3395db16
jcwang111 <jcwang111>
2020-03-01 20:52:00 -0800

    Updates

---
+++ precache-manifest.e98bff9e720c490ce5dc4397d4457f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a91021aced70d94fa57871b371907ef",
+    "url": "/german-articles-quiz/index.html"
+  },
+  {
+    "revision": "0b9b83fb1aa598007f1a",
+    "url": "/german-articles-quiz/static/css/main.506e3ad0.chunk.css"
+  },
+  {
+    "revision": "b76ed4a22f012d2742f1",
+    "url": "/german-articles-quiz/static/js/2.27e614e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/german-articles-quiz/static/js/2.27e614e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9b83fb1aa598007f1a",
+    "url": "/german-articles-quiz/static/js/main.7b52d1a5.chunk.js"
+  },
+  {
+    "revision": "29ef5b845e539a892232",
+    "url": "/german-articles-quiz/static/js/runtime-main.1466c8a8.js"
+  }
+]);
